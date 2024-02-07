b47eabb745fb1cf502de6f8994bf829ed525e9cf
nfeng037 <nfeng@academic.rrc.ca>
2024-02-07 11:59:07 -0600

    Enhance db/seeds.rb to import product and category data from CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,26 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "csv"
 
-676.times do
+# Clean the products and categories tables
+Product.destroy_all
+Category.destroy_all
+
+# Loop through the rows of the CSV file
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |product|
+  category_name = product['category']
+  category = Category.find_or_create_by(name: category_name)
+
   product = Product.new(
-    title: Faker::Commerce.product_name,
-    price: Faker::Commerce.price(range: 0.0..100.0), # 你可以根据需要调整范围
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
+    title: product['name'],
+    price: product['price'],
+    description: product['description'],
+    stock_quantity: product['stock quantity'],
+    category: category
   )
+
   product.save!
 end
